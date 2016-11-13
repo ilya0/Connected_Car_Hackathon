f88af3ebc67590357cd0da0697f61571e08663e1
ilya0 <iosovets@gmail.com>
2016-11-13 13:38:35 -0800

    request route works, needs export and variable location to be fixed but everything else works, logs the history of trips also

--- Backend/config/routes.js
+++ Backend/config/routes.js
@@ -8,85 +8,70 @@ var methodOverride = require('method-override');
 var appjs          = require('../../app');
 var request        = require('request');
 var morgan         = require('morgan');
+var app            = express();
 
+////Exporting variables from app.js not working
 //this should be exporting variable but its not working for some fucking reason
-var mojiotestlocationlong = appjs.mojiotestlocationlong;
-var mojiotestlocationlat = appjs.mojiotestlocationlat;
-var mojiotesttime = appjs.mojiotesttime;
-var bodyholder;
+// var mojiotestlocationlong = appjs.mojiotestlocationlong;
+// var mojiotestlocationlat = appjs.mojiotestlocationlat;
+// var mojiotesttime = appjs.mojiotesttime;
 
+// these export situation needs to be fixed
+var mojiotestlocationlong = -118.34097120000001;
+var mojiotestlocationlat = 34.1020231;
+var mojiotesttime = "7:00 pm";
+
+var bodyholder; // holds the body of the json
+var historyarray = [];
+
+// home page test
 router.get('/', function(req, res, next) {
    console.log("home route hit");
    res.json({message:"car is enroute or maybe not"});
 });
 
 
-router.get('/runprogram', function(req, res, next) {
-   console.log("program initialized");
+
+
+
+//initialize the program
+router.get('/logtrip', function(req, res, next) {
+   console.log(" /runprogram initialized");
    console.log(" long is "+mojiotestlocationlong+" lat is"+mojiotestlocationlat);
-   console.log("running program");
-   getgoogleplacesdata(mojiotestlocationlong,mojiotestlocationlat); //this is running the rest
+  request('https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&rankby=distance&types=food&key=AIzaSyCihzpBz81mq08Dc1g2xCmyqIVKeJQNQwo',
+      function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+
+          bodyholder = JSON.parse(body);
+         // console.log(typeof bodyholder);
+         // console.log(bodyholder.types);
+
+         if (bodyholder.status == 'ZERO_RESULTS'){
+            console.log('no results on that location');
+            res.json({message:'no results on that location'});
+         }else{
+         // console.log(bodyholder);
+          //console.log(bodyholder.results[0].types[0]);
+          res.send(bodyholder.results[0].types[0]);
+
+        // key value push
+    historyarray.push({type:bodyholder.results[0].types[0], Time: mojiotesttime});
+      console.log(historyarray);
+         }
+
+
+        }
+
+      }
+  );
+
 });
+
 
 //this needs be to changed with the test url
 //https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${carlocationlat},{carlocationlong}&radius=500&type=restaurant&keyword=cruise&key=GOOGLE_PLACES_API_KEY`
 
 
-var getgoogleplacesdata = function(carlocationlong, carlocationlat, cartime){
-
-request('https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=34.1020231,-118.34097120000001&radius=10&keyword=points_of_interest&key=AIzaSyCihzpBz81mq08Dc1g2xCmyqIVKeJQNQwo',
-
-  function (error, response, body) {
-  if (!error && response.statusCode == 200) {
-    bodyholder = body;
-    console.log(bodyholder.results.types[0]);// Print the google web page.
-//this is how we would run the right ones
-    //bodyholder.results.types[0]
-
-    // res.json(body);
-  }
-});
-//this will need to be a sort through the json object
-
-            // for(i=0; i< data.results.length;i++){
-            // console.log(data.results[i].name);
-            // $("#div1").append(data.results[i].name);
-            // $(".ulclass").append('<li>'+data.results[i].name+'</li>');
-            // }
-
-
-        };
-
-
-
-
-
-
-// router.get('/', /* someController.method */);
-
-// // Google OAuth
-// router.get(/* some route */, passport.authenticate('google-oauth20'), function(req, res) {
-
-// });
-
-// router.get(/* some route */, passport.authenticate('google-oauth20', { failureRedirect: '/login' }),
-//   function(req, res) {
-//     res.redirect('/');
-// });
-
-
-// function authenticate(req, res, next) {
-//   if(req.user) {
-//     next();
-//   } else {
-//     res.redirect( some route );
-//   }
-// }
-
-// router.get('/logout', function(req, res) {
-//   req.logout();
-//   res.redirect('/');
-// });
 
 
 
