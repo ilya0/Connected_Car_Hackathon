8643e94f7376a65dafa35bee9d2b697c30b003a2
ilya0 <iosovets@gmail.com>
2016-11-12 17:38:47 -0800

    took out netflix stuff

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var router         = express.Router(); //simplifying the router
 var logger         = require( 'morgan' ); //logs the shit into console
 var path           = require('path');
 var http           = require('http'); // Im not sure if I need this I just cant get this fuckign http to link with the js and the css
-var nodeflix       = require('./nodeflix');
+// var nodeflix       = require('./nodeflix');
 // this is the global variable to allow the drone to fly or not
 var shouldifly     = false;
 var favicon        = require('serve-favicon');
@@ -69,52 +69,6 @@ app.use(debugReq);
 app.use('/', routes);
 
 
-// NODEFLIX NETFLIX examples
-var z = new nodeflix({
-  consumer_key: '',
-  consumer_secret: '',
-  oauth_token: '',
-  oauth_token_secret: '',
-  user_id: ''
-});
-
-// get user information (colon prefixed strings will be replaced with matching config option(s) for convenience e.g. ':user_id')
-n.get('/users/:user_id', function() {
-    console.log(this.toJSON());
-});
-
-// lookup something from the people catalog
-n.get('/catalog/people', { term: 'DeNiro' }, function(data) {
-    console.log(data);
-});
-
-// delete something from a user's instant queue
-n.delete('/users/:user_id/queues/instant/saved/60022048', function(data) {
-    if(data.status && data.status.status_code == 200) {
-        console.log('Item successfully deleted!');
-    }
-});
-
-// add something to a user's instant queue
-n.post('/users/:user_id/queues/instant', { title_ref: '/catalog/titles/series/70158331'}, function(data) {
-    if(data.status && data.status.message == 'success') {
-        console.log('Item successfully added to instant queue!')
-    }
-});
-
-// also supports simple "deferred-like" (not a true deferred right now) syntax
-n.get('/catalog/people', { term: 'Brad Pitt'}).end(function() {
-    console.log(this.toJSON());
-});
-
-// chaining is all the rage these days
-n.get('/catalog/people', { term: 'Bruce Willis' }).end(function() {
-    console.log(this.toJSON());
-}).get('/catalog/titles', { term: 'Voyager'}).end(function() {
-    console.log(this.toJSON());
-});
-
-// END OF NODEFLIX NETFLIX examples
 
 
 // Catches all 404 routes.
